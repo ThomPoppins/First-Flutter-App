52a3405e930b3c7b6d62ac9a8d4c416bbe48f28b
Thom Poppins <thompoppins@gmail.com>
2023-09-25 12:36:25 +0100

    Add documentation to main.dart

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # First Flutter App
 
-A new Flutter project.
+My first Flutter application. Build by following [this tutorial](https://codelabs.developers.google.com/codelabs/flutter-codelab-first#0) from Google Codelabs.
 
-## Getting Started
+## Why **Flutter** and not **React Native**?
 
-This project is a starting point for a Flutter application.
+1. Performance: Flutter uses Dart, a compiled language, which allows it to achieve high performance and fast startup times. React Native, on the other hand, uses JavaScript, which can be slower.
 
-A few resources to get you started if this is your first Flutter project:
+2. Hot Reload: Flutter's hot reload feature allows developers to see changes in the app's UI in real-time, without having to restart the app. React Native also has a hot reload feature, but it can be slower and less reliable.
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+3. Customizable Widgets: Flutter provides a rich set of customizable widgets that can be used to build beautiful and responsive UIs. React Native, on the other hand, relies on native components, which can be less customizable.
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+4. Development Time: Flutter's fast development cycle, combined with its hot reload feature, can significantly reduce development time. React Native can also be fast, but it may require more setup and configuration.
+
+5. Single Codebase: Flutter allows developers to build apps for both iOS and Android using a single codebase, which can save time and reduce complexity. React Native also allows for cross-platform development, but it may require more platform-specific code.
+  
--- lib/main.dart
+++ lib/main.dart
@@ -26,23 +26,48 @@ class MyApp extends StatelessWidget {
 }
 
 class MyAppState extends ChangeNotifier {
+  // Initialize the current WordPair
   var current = WordPair.random();
 
+  // Get the next WordPair
   void getNext() {
     current = WordPair.random();
+
+    // Notify listeners that the current WordPair has changed
+    notifyListeners();
+  }
+
+  // Array of WordPair objects
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    // Returns true if the current WordPair is in the favorites array
+    if (favorites.contains(current)) {
+      // Remove the current WordPair from the favorites array
+      favorites.remove(current);
+    } else {
+      // Add the current WordPair to the favorites array
+      favorites.add(current);
+    }
+    // Notify listeners that the favorites array has changed
     notifyListeners();
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  // The build method rebuilds the widget tree when the state changes
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
+    // Scaffold is a widget that provides a default app bar, title, and a body
     return Scaffold(
+      // Center is a widget that centers its child
       body: Center(
+        // Column is a widget that displays its children in a vertical array
         child: Column(
+          // MainAxisAlignment.center centers the children vertically
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             BigCard(pair: pair),
@@ -60,6 +85,7 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
+// A stateless widget that displays a WordPair on a "big" card
 class BigCard extends StatelessWidget {
   const BigCard({
     super.key,
@@ -68,19 +94,35 @@ class BigCard extends StatelessWidget {
 
   final WordPair pair;
 
+  // Rebuild the BigCard widget when the state changes
   @override
   Widget build(BuildContext context) {
+    // `Theme.of(context)` gets the current theme
     var theme = Theme.of(context);
+    // `textTheme` is a collection of text styles
+    // `displaySmall` is a text style that is used for the card text
+    // `copyWith` creates a new text style from the current one
     var style = theme.textTheme.displaySmall!.copyWith(
+      // `colorScheme.onPrimary` is the text color for the current theme
+      // that will fit the best on the primary color
       color: theme.colorScheme.onPrimary,
     );
 
     return Card(
+      // `color` is the background color of the card
       color: theme.colorScheme.primary,
+      // Add padding inside the card
       child: Padding(
+        // `EdgeInsets.all` is shorthand for
+        // `EdgeInsets.symmetric(vertical: 20, horizontal: 20)`
+        // `EdgeInsets.symmetric` is shorthand for
+        // `EdgeInsets.only(top: 20, right: 20, bottom: 20, left: 20)`
         padding: const EdgeInsets.all(20),
         child: Text(
+          // `pair.asPascalCase` converts the WordPair to PascalCase
+          // this is a function from the english_words package
           pair.asPascalCase,
+          // `style` is the text style to use for the text in the card
           style: style,
           // `semanticsLabel` is for accessibility like screen readers to be
           // able to separate the words when all is in lower case:
