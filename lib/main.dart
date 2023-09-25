bd93e30ea0bf5636a023f79611d2b93b2696d42e
Thom Poppins <thompoppins@gmail.com>
2023-09-25 12:19:11 +0100

    Add sizebox for space between button and card and center the button and BigCard horizontally + vertically

--- lib/main.dart
+++ lib/main.dart
@@ -41,17 +41,20 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random word pair:'),
-          BigCard(pair: pair),
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+          ],
+        ),
       ),
     );
   }
